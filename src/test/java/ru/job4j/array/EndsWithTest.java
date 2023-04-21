2967da1e5c4f26021ca2416442aa01c98274ede5
Kiril-k23 <kirillkleyn@gmail.com>
2023-04-21 22:36:58 +0300

    6.4.3. Слово заканчивается на ... [#156317]

--- src/test/java/ru/job4j/array/EndsWithTest.java
+++ src/test/java/ru/job4j/array/EndsWithTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class EndsWithTest {
     @Test
     public void whenEndWithPrefixThenTrue() {
